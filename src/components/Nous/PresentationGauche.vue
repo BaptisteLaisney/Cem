4e3fbdad181f24c2352d05912c557e37f3075247
Baptiste <22005583@etu.unicaen.fr>
2022-02-26 22:21:28 +0100

    debut de qui somme nous

---
+++ src/components/Nous/PresentationGauche.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="PresentationGauche">
+        <img class = "logo-partenaire" :src="require('@/assets/images/Partenaires/' + image)" alt="Image Orga">
+        <div class="droite">
+            <div class="TextPresentation">
+                bdhfskjil  hifhidh uifhiuh ihsidh hiduf hihsd hhui hi hfdsh ihdiu fhuidshui hdsuih fuihui dsfhuif hiush hi hi hi huih hsduiuh ezuis
+            </div>
+
+        </div>
+
+    </div>
+</template>
+<script>
+export default {
+  name: 'PresentationGauche',
+  props: {
+    image: String
+  }
+}
+</script>
+<style scoped>
+.PresentationGauche{
+    background-color: aqua;
+    position: relative;
+    width: 90%;
+    margin: auto;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    padding: 10px;
+}
+.logo-partenaire{
+    height: 30vh;
+}
+.TextPresentation{
+}
+.droite{
+    width: 50%;
+}
+</style>
--- src/views/Nous.vue
+++ src/views/Nous.vue
@@ -1,3 +1,22 @@
 <template>
-    <h1>Qui sommes nous ?</h1>
+    <div class="Nous">
+        <h1>Qui sommes nous ?</h1>
+        <PresentationGauche image="Actif.png"/>
+    </div>
+
 </template>
+<script>
+import PresentationGauche from '@/components/Nous/PresentationGauche'
+
+export default {
+  name: 'Nous',
+  components: {
+    PresentationGauche
+  }
+}
+</script>
+<style scoped>
+.Nous{
+    text-align: center;
+}
+</style>
